7f1c297343880bf592a5d5e5efe43ec51a409b27
Fortzy05 <omorodion.fortune@gmail.com>
2025-03-10 17:13:16 +0100

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <main className="flex justify-center items-center flex-col">
+      <h1 className="text-4xl">CVR Recovery</h1>
+    </main>
+  );
+}
+
+export default App;
