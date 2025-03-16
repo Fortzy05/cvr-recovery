9a4f549edd1211cbc152b6db290c3beb017e392f
Fortzy05 <omorodion.fortune@gmail.com>
2025-03-16 17:52:39 +0100

    gallery and car sales

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 
 function HeroSection() {
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center text-white text-center px-6 uppercase bg-black/20">
-      <h1 className="text-4xl md:text-6xl font-bold">
-        Fast & Reliable Vehicle Recovery
+    <div className="w-full h-full flex flex-col items-center justify-center text-white text-center px-4 bg-black/25">
+      <h1 className="text-3xl sm:text-4xl md:text-6xl font-bold leading-tight">
+        Fast & Reliable <br /> Vehicle Recovery
       </h1>
-      <p className="text-lg mt-4 font-bold">
+      <p className="text-sm sm:text-lg mt-3 sm:mt-4 font-medium sm:font-bold max-w-md">
         24/7 Emergency Assistance | Towing | Transport | Removals
       </p>
       <Link
         to="/booking"
-        className="mt-6 bg-yellow-500 text-gray-900 px-6 py-3 rounded-lg text-lg font-semibold hover:bg-yellow-400"
+        className="mt-5 sm:mt-6 bg-yellow-500 text-gray-900 px-6 py-3 rounded-lg text-base sm:text-lg font-semibold hover:bg-yellow-400 transition-all duration-300"
       >
         Get Assistance Now
       </Link>
